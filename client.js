d7b464d04510e6ddc6a7490cfc49caaf96395441
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-24 02:49:24 +0000

    refactor: code cleanup

--- client.js
+++ client.js
@@ -1,43 +1,22 @@
 const net = require('net');
+const { IP, port } = require('./constants');
 
-//establish a connection with the game server
 const connect = function () {
-  // conn object represents the connection with the server
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    IP,
+    port
   });
 
   conn.setEncoding("utf8");
-
-  // display message from server when data event occurs
-  conn.on('data', (message) =>{
+  conn.on('data', (message) => {
     console.log('Server says: ', message);
   });
 
-  conn.on('connect', () =>{
+  conn.on('connect', () => {
     console.log('Successfully connected to game server');
-    //send server username
+    console.log('Game Started!')
     conn.write('Name: Moo');
-    //move up one block
-    
-    setInterval(() =>{
-      conn.write('Move: up');
-    },500);
-    setInterval(() =>{
-      conn.write('Move: right');
-    },1000);
-    setInterval(() =>{
-      conn.write('Move: down');
-    },1500);
-    setInterval(() =>{
-      conn.write('Move: left');
-    }, 2000);
   });
-
-  
-
-  
 
   return conn;
 };
