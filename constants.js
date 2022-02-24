0c65e16afb1ab7cb9f758bb171dd97dfbd325ca4
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-24 18:04:44 +0000

    refactor: make WSAD constants

--- constants.js
+++ constants.js
@@ -1,25 +1,23 @@
+
+
 const IP = 'localhost';
-const port = 50541;
-const MOVE_UP_KEY = 'w';
-const MOVE_LEFT_KEY = 'a';
-const MOVE_DOWN_KEY = 's';
-const MOVE_RIGHT_KEY = 'd';
+const PORT = 50541;
 const CTRL_C = '\u0003';
 
-const CANNED_MESSAGES = {
+const  KEY_STROKES= {
+  'w': 'Move: up',
+  'a': 'Move: left',
+  's': 'Move: down',
+  'd': 'Move: right',
   1: 'Say: Let the games begin',
   2: 'Say: Better luck next time',
   3: 'Say: GG',
   4: 'Say: That was fun'
 }
 
-module.exports = { 
+module.exports = {
   IP, 
-  port,
+  PORT,
   CTRL_C,
-  MOVE_UP_KEY,
-  MOVE_LEFT_KEY,
-  MOVE_DOWN_KEY,
-  MOVE_RIGHT_KEY,
-  CANNED_MESSAGES
+  KEY_STROKES
 };
