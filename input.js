c2e6a578fdebeb4798d4642c2c231c267d40f143
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-24 18:03:22 +0000

    refactor: removed switch statement

--- input.js
+++ input.js
@@ -1,8 +1,6 @@
-const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, CANNED_MESSAGES, CTRL_C} = require('./constants');
-let connection; //to store conn object from client
+const { KEY_STROKES, CTRL_C} = require('./constants');
+let connection; 
 
-
-//setup interface to handle user input from stdin
 const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
@@ -13,33 +11,13 @@ const setupInput = function(conn) {
   return stdin;
 };
 
-
-
-// Callback function that terminates game when ctrl+c is pressed
 const handleUserInput = function(input) {
-
-  // Handles WSAD input form stdin
-  switch (input) {
-  case CTRL_C:
+  
+  if(input === CTRL_C)
     process.exit();
-  case MOVE_UP_KEY:
-    connection.write('Move: up');
-    break;
-  case MOVE_LEFT_KEY:
-    connection.write('Move: left');
-    break;
-  case MOVE_DOWN_KEY:
-    connection.write('Move: down');
-    break;
-  case MOVE_RIGHT_KEY:
-    connection.write('Move: right');
-    break;
-  default:
-    break;
-  }
 
-  if (CANNED_MESSAGES[input]) {
-    connection.write(CANNED_MESSAGES[input]);
+  if (KEY_STROKES[input]) {
+    connection.write(KEY_STROKES[input]);
   }
 };
 
