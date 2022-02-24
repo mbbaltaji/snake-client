629208a3fb1671dab2877d65a39fdc4d2bb944ad
Mustapha Baltaji <mbbaltaji@gmail.com>
2022-02-24 00:12:23 +0000

    Refactor: move setupInput to input.js file

--- play.js
+++ play.js
@@ -1,26 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('connecting ...');
 connect();
 
-//setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  
-  ///handle user input received from stdin (keyboard) on data event
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-// Callback function that terminates game when ctrl+c is pressed
-const handleUserInput = function(input) {
-  if(input === '\u0003'){
-    process.exit();
-  }
-}
-
 setupInput();
-
